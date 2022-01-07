2451d6a0a75c55347a34326d0030fc436dad7d9a
Oil <oil@Oils-MacBook-Pro.local>
2022-01-07 16:51:01 +0700

    full version

--- redux/actions/todos.action.ts
+++ redux/actions/todos.action.ts
@@ -1,26 +1,27 @@
+import axios from 'axios';
 import { Dispatch } from 'redux';
 import { Todo } from '../../types/todo';
 import { ADD_TODO, GET_ALL_TODO, DELETE_TODO } from '../type';
 
-const setTodo = (payload: any) => ({
-  type: ADD_TODO,
-  payload,
-});
-
 const addTodo = ({ title, subtitle }: Todo) => {
-  return (dispatch: Dispatch) => {
-    setTimeout(() => {
-      dispatch(setTodo({ id: Math.floor(Math.random() * 1000), title, subtitle }));
-    }, 500);
+  return async (dispatch: Dispatch) => {
+    const res = await axios.post('http://localhost:3000/todo', { title, subtitle });
+    return dispatch({ type: ADD_TODO, payload: res.data });
   };
 };
 
 const getAllTodo = () => {
-  return { type: GET_ALL_TODO, payload: null };
+  return async (dispatch: Dispatch) => {
+    const res = await axios.get('http://localhost:3000/todo');
+    return dispatch({ type: GET_ALL_TODO, payload: res.data });
+  };
 };
 
 const deleteById = (id: number) => {
-  return { type: DELETE_TODO, payload: { id } };
+  return async (dispatch: Dispatch) => {
+    const res = await axios.delete(`http://localhost:3000/todo/${id}`);
+    return dispatch({ type: DELETE_TODO, payload: { id } });
+  };
 };
 
 const exported = {
--- redux/reducers/todos.reducer.ts
+++ redux/reducers/todos.reducer.ts
@@ -6,12 +6,7 @@ const initialState: Todo[] = [];
 export function TodosReducer(state = initialState, { type, payload }: any) {
   switch (type) {
     case GET_ALL_TODO:
-      return [
-        ...payload,
-        // { id: 123, title: 'ttt', subtitle: 'mock data' },
-        // { id: 1234, title: 'rrr', subtitle: 'mock data 2' },
-        // { id: 12345, title: 'eee', subtitle: 'mock data 3' },
-      ];
+      return payload;
 
     case ADD_TODO:
       return [...state, payload];
